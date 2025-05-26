e46ed41ccef1ae8c3653ba4cc732fdd8072b9f5f
evilcorp2019 <52998774+evilcorp2019@users.noreply.github.com>
2025-05-27 00:38:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/csv-schema-comparison-tool/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/schema_compare/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
